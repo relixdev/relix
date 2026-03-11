e9939dfef2d180b178ba42600568004168e5c92a
Zach Forsyth <zachforsyth@Mac.shadow.strike>
2026-03-11 14:37:12 -0700

    feat(protocol): add protocol version constants

---
+++ protocol/version.go
@@ -0,0 +1,9 @@
+package protocol
+
+const (
+	// ProtocolVersion is the current wire protocol version.
+	ProtocolVersion = 1
+
+	// MinSupportedVersion is the oldest version this code can handle.
+	MinSupportedVersion = 1
+)
